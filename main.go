cadd70f295fc7a1919646a8f7beb26d5e95fd2f5
Adrian Lehmann <info@adrianlehmann.net>
2021-04-12 13:09:31 -0400

    Remove finished todo

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/req/{id}", vaccinepassport.GetRequest).Methods(http.MethodGet)
 	r.HandleFunc("/req", vaccinepassport.RequestPassport).Methods(http.MethodPost)
-	r.HandleFunc("/ret/{id}", vaccinepassport.RetrievePassport).Methods(http.MethodGet) // TODO: Make get and remove body
+	r.HandleFunc("/ret/{id}", vaccinepassport.RetrievePassport).Methods(http.MethodGet)
 	r.HandleFunc("/sign/{id}", vaccinepassport.SignVaccineData).Methods(http.MethodPost)
 	http.ListenAndServe(":8010", r)
 }
