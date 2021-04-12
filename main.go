5757d32ef0ae93cc2bff6b4e88f070815628c075
Adrian Lehmann <info@adrianlehmann.net>
2021-04-12 12:58:35 -0400

    Fix route sign

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	r.HandleFunc("/req/{id}", vaccinepassport.GetRequest).Methods(http.MethodGet)
 	r.HandleFunc("/req", vaccinepassport.RequestPassport).Methods(http.MethodPost)
 	r.HandleFunc("/ret/{id}", vaccinepassport.RetrievePassport).Methods(http.MethodGet) // TODO: Make get and remove body
-	r.HandleFunc("/sign", vaccinepassport.SignVaccineData).Methods(http.MethodPost)
+	r.HandleFunc("/sign/{id}", vaccinepassport.SignVaccineData).Methods(http.MethodPost)
 	http.ListenAndServe(":8010", r)
 }
